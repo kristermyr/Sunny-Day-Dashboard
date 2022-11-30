86604001dd62a64c287af7f4476ef9912f3eb14a
kristermyr <krister90@gmail.com>
2022-11-30 15:46:01 -0600

    connected with weather API and getting results back in console of city typed in by user!

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 var apiKey = "9876abe1ec13a72a4e7d542293d7c7b0";
-var inputValue = document.getElementById('#cityinput');
+var inputValue = document.getElementById('cityinput');
 
 
 var button = document.querySelector('.btn');
@@ -8,8 +8,8 @@ var name = document.querySelector('.name');
 var desc = document.querySelector('.desc');
 var temp = document.querySelector('.temp');
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + inputValue.innerHTML + "&appid=" + apiKey;
-console.log(inputValue.innerHTML)
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + inputValue.innerText + "&appid=" + apiKey;
+
 /*button.addEventListener('click', function(){
     fetch(queryURL)
     .then(response => response.json())
@@ -19,12 +19,23 @@ console.log(inputValue.innerHTML)
 
 })
  */  
- $('.search').on("click", function (event){
-    event.preventDefault();
-    fetch(queryURL)
-    .then(response => response.json())
-    .then(data => console.log(data))
- 
+    button.addEventListener('click',function(){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + inputValue.value + "&appid=" + apiKey).then((response) =>{ 
+         response.json().then((data) => {
+            console.log(data);
+         })
+    });
+
+      /*      .then((res) => {
+                if (res.status !==200) {
+                console.log('Problem!', res.status);
+                }
+                 res.json().then((data)=> {
+             console.log(data);
+              });
+           })
+    
+    */
 
 });
   
