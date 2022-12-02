ab3a2286718e1920835d6d51dc56e70313eef067
kristermyr <krister90@gmail.com>
2022-12-02 11:05:22 -0600

    problems with connecting info in console to page, contacting bcs

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,13 @@
 var apiKey = "9876abe1ec13a72a4e7d542293d7c7b0";
 var inputValue = document.getElementById('cityinput');
-
+var timeDisplayEl = $('#date1')
 
 var button = document.querySelector('.btn');
 //var inputValue = document.querySelector('textValue');
 var name = document.querySelector('.name');
 var desc = document.querySelector('.desc');
 var temp = document.querySelector('.temp');
+var mph = "mph";
 
 // var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + inputValue.innerText + "&appid=" + apiKey;
 
@@ -19,14 +20,26 @@ var temp = document.querySelector('.temp');
 
 })
  */  
+
+function displayTime(){                                             
+  var reformatDate = dayjs().format('dddd, MMMM D, YYYY');
+  timeDisplayEl.text(reformatDate);
+  }
+  setInterval(displayTime, 
+    
+    1000);
+
+
     button.addEventListener('click',function(){
         
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + inputValue.value + "&appid=" + apiKey).then((response) =>{ 
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + inputValue.value + "&appid=" + apiKey)
+        .then((response) =>{ 
          response.json().then((data) => {
             var lat = data.coord.lat
             var long = data.coord.lon
         fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${apiKey}`)
-        .then(response=>response.json()).then(day5data=>{
+        .then(response=>response.json())
+        .then(day5data=>{
             
             document.querySelector(".cardTodayCityName").innerHTML=day5data.city.name;
             //document.querySelector(".todayTemp").innerHTML=day5data.temp
@@ -35,8 +48,10 @@ var temp = document.querySelector('.temp');
                 var singleDay = day5data.list[i]
                 console.log(singleDay.weather[0].description)
                 console.log(singleDay)
-                document.querySelector('.todayTemp').innerHTML=singleDay.main.temp
-                document.querySelector('.description').innerHTML=singleDay.weather[0].description
+                document.querySelector('.todayTemp').innerHTML=singleDay.main.temp;
+                document.querySelector('.description').innerHTML=singleDay.weather[0].description;
+                console.log(singleDay.wind.speed)
+                document.querySelector('#wind0').innerHTML=singleDay.wind.speed;
             }
         })
             
