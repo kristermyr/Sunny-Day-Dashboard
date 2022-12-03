5a5affa5b3a973015edf6d9b5f9bb03aeaf16e7d
kristermyr <krister90@gmail.com>
2022-12-03 10:44:31 -0600

    working on local storage. asking BSC for help to convert listener to function

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,12 @@
 var apiKey = "9876abe1ec13a72a4e7d542293d7c7b0";
 var inputValue = document.getElementById('cityinput');
 var timeDisplayEl = $('#date1')
-
 var button = document.querySelector('.btn');
-//var inputValue = document.querySelector('textValue');
 var name = document.querySelector('.name');
 var desc = document.querySelector('.desc');
 var temp = document.querySelector('.temp');
 var mph = "mph";
 
-
-// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + inputValue.innerText + "&appid=" + apiKey;
-
-/*button.addEventListener('click', function(){
-    fetch(queryURL)
-    .then(response => response.json())
-    .then(data => console.log(data)) 
- 
-    
-
-})
- */  
 
 function displayTime(){                                             
   var reformatDate = dayjs().format('dddd, MMMM D, YYYY');
@@ -39,7 +25,8 @@ for  (var i = 0; i < 5; i++) {
 }
 
 
-console.log(fiveDay)
+console.log(fiveDay);
+
     function convertion(val){
         return (val - 273).toFixed(2)
     }
@@ -56,8 +43,8 @@ console.log(fiveDay)
         .then(response=>response.json())
         .then(day5data=>{
             
+          //  var icon = day5data.weather[0].main.temp
             
-//current day
             document.querySelector(".cardTodayCityName").innerHTML=day5data.city.name;
           //  document.querySelector('#fimg').innerHTML=
             document.querySelector("#temp").innerHTML=`<span>${ convertion(day5data.list[0].main.temp )} C</span>`;
@@ -75,51 +62,34 @@ console.log(fiveDay)
 
             //Day2
             document.querySelector("#date01").innerHTML=fiveDay[1];
-            document.querySelector("#temp1").innerHTML=`<span>${ convertion(day5data.list[2].main.temp )} C</span>`;
-            document.querySelector('#Humidity1').innerHTML=`<span>${day5data.list[2].main.humidity} %<span>`;
-            document.querySelector('#Wind1').innerHTML=`<span>${day5data.list[2].wind.speed} m/s<span>`;
+            document.querySelector("#temp1").innerHTML=`<span>${ convertion(day5data.list[6].main.temp )} C</span>`;
+            document.querySelector('#Humidity1').innerHTML=`<span>${day5data.list[6].main.humidity} %<span>`;
+            document.querySelector('#Wind1').innerHTML=`<span>${day5data.list[6].wind.speed} m/s<span>`;
             
             //Day3
             document.querySelector("#date2").innerHTML=fiveDay[3];
-            document.querySelector("#temp2").innerHTML=`<span>${ convertion(day5data.list[3].main.temp )} C</span>`;
-            document.querySelector('#Humidity2').innerHTML=`<span>${day5data.list[3].main.humidity} %<span>`;
-            document.querySelector('#Wind2').innerHTML=`<span>${day5data.list[3].wind.speed} m/s<span>`;
+            document.querySelector("#temp2").innerHTML=`<span>${ convertion(day5data.list[14].main.temp )} C</span>`;
+            document.querySelector('#Humidity2').innerHTML=`<span>${day5data.list[14].main.humidity} %<span>`;
+            document.querySelector('#Wind2').innerHTML=`<span>${day5data.list[14].wind.speed} m/s<span>`;
 
             //Day4
             document.querySelector("#date3").innerHTML=fiveDay[3];
-            
+            document.querySelector("#temp3").innerHTML=`<span>${ convertion(day5data.list[22].main.temp )} C</span>`;
+            document.querySelector('#Humidity3').innerHTML=`<span>${day5data.list[22].main.humidity} %<span>`;
+            document.querySelector('#Wind3').innerHTML=`<span>${day5data.list[22].wind.speed} m/s<span>`;
 
             //Day5
             document.querySelector("#date4").innerHTML=fiveDay[4];
-
-          /*  for(var i = 0; i< day5data.list.length; i=(i+7)){
-                var singleDay = day5data.list[i]
-                console.log(singleDay.weather[0].description)
-                console.log(singleDay)
-                document.querySelector('.todayTemp').innerHTML=singleDay.main.temp;
-                document.querySelector('.description').innerHTML=singleDay.weather[0].description;
-                console.log(singleDay.wind.speed)
-                document.querySelector('#wind0').innerHTML=singleDay.wind.speed;
-            }*/
-
-           // var day5WeatherDetails = ['Temp: ' + weatherData.current.temp + ' °C', 'Wind: ' + weatherData.current.wind_speed + ' km/h', 'Humidity: ' + weatherData.current.humidity + '%', 'UV Index: ' + weatherData.current.uvi]
+            document.querySelector("#temp4").innerHTML=`<span>${ convertion(day5data.list[30].main.temp )} C</span>`;
+            document.querySelector('#Humidity4').innerHTML=`<span>${day5data.list[30].main.humidity} %<span>`;
+            document.querySelector('#Wind4').innerHTML=`<span>${day5data.list[30].wind.speed} m/s<span>`;
+            
             
 
-           
-
-
-    }
-    )
+                });               
+                    });        
+                });
+            });
         
-         
-               
-         })
-         
-    });
-
-      
-
-});
-  
-  
-
+    
+    
